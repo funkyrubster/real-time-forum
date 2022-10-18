90fc370239308450b3ad47015d347200a3cea687
morubie <mo.rubie@gmail.com>
2022-10-18 22:24:54 +0100

    handle_example

--- main.go
+++ main.go
@@ -16,10 +16,26 @@ func main() {
 
 	http.Handle("/static/", http.StripPrefix("/static/", path2))
 
-	http.HandleFunc("/", handlers.IndexHandler) //not created yet
+	http.HandleFunc("/test", handlers.TestHandler)
 
 	fmt.Println("Server started at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	sqldb.CloseDB()
 }
+
+// func main() {
+// 	sqldb.ConnectDB()
+// 	// sqldb.CreateDB() not creates yet
+
+// 	path2 := http.FileServer(http.Dir("static"))
+
+// 	http.Handle("/static/", http.StripPrefix("/static/", path2))
+
+// 	http.HandleFunc("/", handlers.IndexHandler) //not created yet
+
+// 	fmt.Println("Server started at port 8080")
+// 	log.Fatal(http.ListenAndServe(":8080", nil))
+
+// 	sqldb.CloseDB()
+// }
