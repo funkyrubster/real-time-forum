56a61ed5b96a83e29c333bd29cca04e14c042995
morubie <mo.rubie@gmail.com>
2022-10-28 13:26:28 +0100

    chatimplement

--- handlers/handle_homepage.go
+++ handlers/handle_homepage.go
@@ -15,10 +15,10 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		PostScroll []*PostRaw
 	}
 	// var post *Pitem
-	tpl, err := template.ParseGlob("index.html")
+	tpl, err := template.ParseFiles("../templates/login.html")
 	if err != nil {
 		http.Error(w, "500 Internal Server Error, Parsing Error", http.StatusInternalServerError)
-		WarnMessage(w, "500 Internal Server Error, Parsing Error")
+		//WarnMessage(w, "500 Internal Server Error, Parsing Error")
 		return
 	}
 	if r.URL.Path != "/" {
