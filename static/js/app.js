3b732a7a4eda71e962cdf10e095fd79e048b3fd4
Connor <hello@connorhanks.com>
2022-11-11 22:37:56 +0000

    removed DRY comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,20 +1,20 @@
-// AUTHENTICATION
 function showRegistrationUI() {
-    // Shows #registration and hides #login
     document.querySelector("#registration").style.display = "flex";
     document.querySelector("#login").style.display = "none";
 }
 
 function showLoginUI() {
-    // Shows #login and hides #registration
     document.querySelector("#login").style.display = "flex";
     document.querySelector("#registration").style.display = "none";
 }
 
-// CHAT
 function showChat() {
-    // Shows #chat and hides #login and #registration
     document.querySelector("#chat").style.display = "block";
     document.querySelector("#login").style.display = "none";
     document.querySelector("#registration").style.display = "none";
+}
+
+function showFeed() {
+    document.querySelector(".auth-container").style.display = "none";
+    document.querySelector("main").style.display = "block";
 }
