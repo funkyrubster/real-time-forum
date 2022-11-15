6b9e3065272a23e5304bafd1c2dd843bc2246315
Connor <hello@connorhanks.com>
2022-11-15 16:59:21 +0000

    updated notiifcations to use Notyf

--- static/js/authentication.js
+++ static/js/authentication.js
@@ -1,6 +1,9 @@
 const signUpData = document.getElementById("sign-up-form");
 
-signUpData.addEventListener("submit", function(){
+// Create an instance of Notyf
+var notyf = new Notyf();
+
+signUpData.addEventListener("submit", function () {
   let user = {
     firstname: document.getElementById("firstName").value,
     lastname: document.getElementById("lastName").value,
@@ -8,72 +11,68 @@ signUpData.addEventListener("submit", function(){
     newusername: document.getElementById("newusername").value,
     age: document.getElementById("age").value,
     gender: document.getElementById("gender").value,
-    newpassword: document.getElementById("newpassword").value,
+    newpassword: document.getElementById("newpassword").value
   };
 
   let options = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(user),
+    body: JSON.stringify(user)
   };
 
-  let fetchRes = fetch('http://localhost:8080/register', options);
+  let fetchRes = fetch("http://localhost:8080/register", options);
   fetchRes
-  .then((response) => {
-    if (response.status == '200'){
-     // show alert pop up  successfully created account
-     alert("You have registered successfully")
-      showLoginUI()
-    }
-    else{
-      // pop up unsuccessfull
-      alert("Email or username already exists")
-      console.log("Email or username already exists");
-    }
-    return response.text();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+    .then((response) => {
+      if (response.status == "200") {
+        // show alert pop up  successfully created account
+        notyf.success("You have successfully registered.");
+        showLoginUI();
+      } else {
+        // pop up unsuccessfull
+        notyf.error("The email or username already exists.");
+        console.log("Email or username already exists");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 });
-
-
 
 const loginData = document.getElementById("login-form");
 
-loginData.addEventListener("submit", function(){
+loginData.addEventListener("submit", function () {
   let user = {
     username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-  }
+    password: document.getElementById("password").value
+  };
 
   let options = {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(user),
+    body: JSON.stringify(user)
   };
 
   let fetchRes = fetch("http://localhost:8080/login", options);
   fetchRes
-  .then((response) => {
-    if (response.status == "200"){
-      // add alert login ok
-      alert("You have successfully logged in")
-      showFeed()
-    }
-    else{
-      // add alert  not ok
-      alert("You inputted incorrect details")
-      console.log("not ok");
-    }
-    return response.text();
-  })
-  .then((data) => {
-    
-    console.log(data);
-  });
+    .then((response) => {
+      if (response.status == "200") {
+        // add alert login ok
+        notyf.success("You have logged in successfully.");
+        // alert("You have successfully logged in");
+        showFeed();
+      } else {
+        // add alert  not ok
+        notyf.error("The login details you entered are incorrect.");
+        console.log("not ok");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      console.log(data);
+    });
 });
