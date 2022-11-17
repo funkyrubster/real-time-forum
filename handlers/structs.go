c31bed40260a2146c1802be0fcdb49f949fc2552
Your Name <you@example.com>
2022-11-17 15:27:21 +0000

    Session stored in DB & browser

--- handlers/structs.go
+++ handlers/structs.go
@@ -1,11 +1,5 @@
 package handlers
 
-import (
-	"time"
-)
-
-// var tpl *template.Template
-
 type RegisterData struct {
 	Firstname string `json:"firstName"`
 	Lastname  string `json:"lastName"`
@@ -37,30 +31,8 @@ type UserSession struct {
 	max_age int
 }
 
-// 	// CreationDate time.Time
-// }
-
-// var user_session Cookie
-
-// var CurrentUser User
-
 var Warning struct {
 	Warn string
-}
-
-// each session contains the username of the user and the time at which it expires
-// type Session struct {
-// 	UserID      int
-// 	username    string
-// 	sessionName string
-// 	sessionUUID string
-// 	expiry      time.Time
-// }
-
-type Cookie struct {
-	Name    string
-	Value   string
-	Expires time.Time
 }
 
 type Pitem struct {
