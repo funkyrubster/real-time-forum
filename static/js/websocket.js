e64152c170d8f650819e05fa01b2fbf06517a3a0
Connor <hello@connorhanks.com>
2022-11-24 00:05:57 +0000

    Revert commit: 002d306

--- static/js/websocket.js
+++ static/js/websocket.js
@@ -1,50 +1,22 @@
-var conn;
-var msg = document.getElementById("msg");
-var log = document.getElementById("log");
+// export const createWebsocket = () => {
+//   return new WebSocket("ws://localhost:8080/ws");
+// };
 
-function appendLog(item) {
-  var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
-  log.appendChild(item);
-  if (doScroll) {
-    log.scrollTop = log.scrollHeight - log.clientHeight;
-  }
-}
+// let socket = createWebsocket();
 
-document.getElementById("form").onsubmit = function () {
-  if (!conn) {
-    return false;
-  }
-  if (!msg.value) {
-    return false;
-  }
-  conn.send(msg.value);
-  msg.value = "";
-  return false;
-};
+// console.log("Attempting websocket connection...");
 
-if (window["WebSocket"]) {
-  conn = new WebSocket("ws://" + document.location.host + "/ws");
-  conn.onclose = function (evt) {
-    var item = document.createElement("div");
-    item.innerHTML = "<b>Connection closed.</b>";
-    appendLog(item);
-  };
-  conn.onmessage = function (evt) {
-    var messages = evt.data.split('\n');
-    for (var i = 0; i < messages.length; i++) {
-      var item = document.createElement("div");
-      item.innerText = messages[i];
-      appendLog(item);
-    }
-  };
-} else {
-  var item = document.createElement("div");
-  item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-  appendLog(item);
-}
+// socket.onopen = () => {
+//   console.log("Successfully connected.");
+// };
 
-let messengerButton = document.getElementById("messenger");
-messengerButton.addEventListener("click", () => {
-  document.getElementById("log").style.display = "block";
-  document.getElementById("form").style.display = "block";
-})
+// socket.onmessage = (event) => {
+//   console.log(event.data);
+// };
+// socket.onclose = (event) => {
+//   console.log("Socket closed connection:", event);
+// };
+
+// socket.onerror = (error) => {
+//   console.error("Socket error:", error);
+// };
