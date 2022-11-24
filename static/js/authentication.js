3d58c5a50dfe3847f8dc31d1d339f378a01aa796
Connor <hello@connorhanks.com>
2022-11-24 01:24:55 +0000

    added formatting for dates

--- static/js/authentication.js
+++ static/js/authentication.js
@@ -1,5 +1,86 @@
 const signUpData = document.getElementById("sign-up-form");
 
+function convertDate(date) {
+  // Seperate year, day, hour and minutes
+  let yyyy = date.slice(0, 4);
+  let dd = date.slice(8, 10);
+  let hh = date.slice(11, 13);
+  let mm = date.slice(14, 16);
+
+  // Get int for day of the week (0-6, Sunday-Saturday)
+  const d = new Date(date);
+  let dayInt = d.getDay();
+  let day = "";
+  switch (dayInt) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+  }
+
+  // Get int for month (0-11, January-December)
+  let monthInt = d.getMonth();
+  let month = "";
+  switch (monthInt) {
+    case 0:
+      month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+  }
+
+  fullDate = day + ", " + dd + " " + month + ", " + yyyy + " @ " + hh + ":" + mm;
+  return fullDate;
+}
+
 // Create an instance of Notyf
 var notyf = new Notyf();
 
@@ -130,7 +211,7 @@ function displayPosts(data) {
       data.CreatedPosts[i].username +
       `</p>
             <p class="timestamp">` +
-      data.CreatedPosts[i].CreatedAt +
+      convertDate(data.CreatedPosts[i].CreatedAt) +
       `</p>
           </div>
         </div>
