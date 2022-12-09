963ad87d3fba55d7d78a65a47b05a5833e8703ec
klaudia.bartkowska <klaudiabarti@gmail.com>
2022-12-09 13:37:03 +0000

    logOut

--- main.go
+++ main.go
@@ -29,6 +29,7 @@ func main() {
 	http.HandleFunc("/register", data.RegistrationHandler)
 	http.HandleFunc("/post", data.Post)
 	http.HandleFunc("/comment", data.Comment )
+	http.HandleFunc("/logout", data.LogoutUser)
 	http.HandleFunc("/getPosts", data.SendLatestPosts)
 	http.HandleFunc("/getHashtags", data.SendLatestHashtags)
 	http.HandleFunc("/updateHashtag", data.UpdateHashtag)
