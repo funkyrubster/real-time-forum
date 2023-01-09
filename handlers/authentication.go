b1982bc0422e85ee61a756a8eff3a6b8e8317866
klaudia.bartkowska <klaudiabarti@gmailcom>
2023-01-09 14:35:22 +0000

    check cookie value with db

--- handlers/authentication.go
+++ handlers/authentication.go
@@ -27,6 +27,27 @@ func (data *Forum) Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (data *Forum) CheckCookie(w http.ResponseWriter, r *http.Request) {
+
+	var cookieValue CookieValue
+
+	// Decode the JSON data from the request body into the comment variable
+	json.NewDecoder(r.Body).Decode(&cookieValue)
+
+	u := data.GetSession(cookieValue.CookieValue)
+	userName := (u.username)
+
+	userInfo :=  data.GetUserProfile(userName)
+
+	js, err := json.Marshal(userInfo)
+	if err != nil {
+		log.Fatal(err)
+	} 
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(js))
+}
+
+
 func (data *Forum) SendLatestActivity(w http.ResponseWriter, r *http.Request) {
 	// Send user information back to client using JSON format
 	onlineactivity := OnlineActivity{
