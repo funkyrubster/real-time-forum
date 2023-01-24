12f5614bd85bc50653dc1b28a4aa314314dba473
klaudia.bartkowska <klaudiabarti@gmail.com>
2023-01-24 17:05:47 +0000

    revert version + offline chat working

--- static/js/app.js
+++ static/js/app.js
@@ -19,25 +19,59 @@ function showChat() {
   document.querySelector("#login").style.display = "none";
   document.querySelector("#registration").style.display = "none";
 }
+
 var conn;
 var msg = document.getElementById("msg");
 var log = document.getElementById("log");
 
 function appendLog(item) {
+
   var doScroll = log.scrollTop > log.scrollHeight - log.clientHeight - 1;
   log.appendChild(item);
   if (doScroll) {
     log.scrollTop = log.scrollHeight - log.clientHeight;
   }
 }
+
 document.getElementById("form").onsubmit = function () {
+  console.log("form");
   if (!socket) {
     return false;
   }
   if (!msg.value) {
     return false;
   }
-  socket.send(msg.value);
+
+  var newDate = new Date();
+  let sendername = document.querySelector(".username").textContent;
+  let userID = document
+    .querySelector("#chat > div.profile-header > div > p")
+    .getAttribute("data-reciverid");
+  let senderID = document.querySelector("p.username").dataset.userid;
+  let msgObj = {
+    message: msg.value,
+    messagesender: sendername,
+    messagerecipient: userID,
+    SenderID: senderID,
+    createdAt: newDate,
+  };
+  console.log("Our Message Object: ", msgObj);
+  var item = document.querySelector("#log");
+  console.log("item from form onsubmit");
+  console.log(msgObj.createdAt);
+  document.querySelector("#log").innerHTML += `
+  <div class="bubbleWrapper">
+    <div class="inlineContainer own">
+      <div class="ownBubble own">
+        ${msgObj.message}
+      </div>
+    </div>
+    <span class="own"> 
+    ${newDate.getHours()}:${newDate.getMinutes()}
+    </span>
+  </div>`;
+  socket.send(JSON.stringify(msgObj));
+
   msg.value = "";
   return false;
 };
@@ -49,13 +83,59 @@ function showFeed() {
     console.log("Socket open", socket);
   };
   socket.onmessage = function (evt) {
-    var messages = evt.data.split("\n");
-    console.log(messages);
-    for (var i = 0; i < messages.length; i++) {
-      var item = document.createElement("div");
-      item.innerText = messages[i];
-      appendLog(item);
+    console.log("here");
+    var messages = JSON.parse(evt.data);
+    console.log("receiving msg", messages);
+
+    let chatrec = document.querySelector(
+      "body > main > div.main > div.left > div.mini-profile > div > p.username"
+    );
+    let chatReceiver = document.querySelector("#chatReceiver");
+    
+    console.log(chatReceiver);
+
+    var item = document.querySelector("#log");
+    chatDiv = document.querySelector(".chat");
+    
+    console.log("xxxxx", messages.messagesender.slice(1), chatReceiver.innerHTML);
+    console.log(messages.messagesender.slice(1) === chatReceiver.innerHTML);
+
+    let onlineusers = Array.from(document.querySelectorAll(".notification"))
+    console.log("online users",onlineusers);
+    for (let i = 0; i < onlineusers.length; i++) {
+      console.log(onlineusers[i].messages);
+      if (onlineusers[i].id == messages.messagesender.slice(1) + "-notification") {
+    
+        ////////////notification
+
+        alert("notif")
+        // 
+      }
     }
+    // if (messages.messagesender.slice(1) === chatReceiver.innerHTML) {
+    //   item.innerHTML +=
+    //     `  <div class="bubbleWrapper">
+    //       <div class="inlineContainer own">
+    //        <div class="ownBubble own">${messages.message} </div>
+    //     </div><span class="own">` +
+    //     convertTime(messages.createdAt) +
+    //     `</span>
+    //     </div>`;
+    // }
+    if (messages.messagesender.slice(1) === chatReceiver.innerHTML) {
+      item.innerHTML += `
+        <div class="bubbleWrapper">
+          <div class="inlineContainer">
+              <div class="otherBubble other">
+               ${messages.message}
+              </div>
+          </div>
+          <span class="other">
+            ${convertTime(messages.createdAt)}
+          </span>
+        </div>`;
+    }
+    // appendLog(item);
   };
 }
 
